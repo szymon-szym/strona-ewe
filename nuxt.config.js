bd8b827e6d170213fc209a48b3b933206292ee83
szymon <szymon.mich@gmail.com>
2019-06-02 20:40:05 +0200

    fix site tile

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: `małe warsztaty`,
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
